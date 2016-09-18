f26dd5b4983490f1eed2f966199d94c5dd023d26
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-09-18 10:24:37 +0200

    Initial commit

---
+++ libsmart3.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+import os
+
+DEBUG = False
+leaf = os.path.realpath(__file__).split('/')[-2]
+
+class SmartDisk():
+  """
+  A class to access information on S.M.A.R.T. disks.
+  This relies on a seperate script to read the S.M.A.R.T. data and store it in
+  the directory `/tmp/' + leaf + '`
+
+  Usage:
+    from libsmart3 import SmartDisk
+
+    see example code at bottom of this file.
+  """
+  def __init__(self, diskid):
+    self.diskid   = "/tmp/" + leaf + "/smartinfo-" + diskid
+    self.id       = diskid
+    self.vars     = "-"
+    self.health   = "-"
+    self.selftest = "-"
+    self.identity = self.cat(self.diskid + "-i.dat").splitlines()
+    retm = retd = rets = ""
+    for line in self.identity:
+      if DEBUG:
+        print(line)
+      if (line != ''):
+        ls = line.split()
+        if (ls[0] == "Model"):
+          retm = line.split(': ')[1].strip()
+        if (ls[0] == "Device") and (ls[1] == "Model:"):
+          retd = line.split(': ')[1].strip()
+        if (ls[0] == "Serial"):
+          rets = line.split(': ')[1].strip()
+    self.identity = retm + " || " + retd + " (" + rets + ")"
+
+  def smart(self):
+    self.vars     = self.cat(self.diskid + "-A.dat").splitlines()
+    self.health   = self.cat(self.diskid + "-H.dat")
+    self.selftest = self.cat(self.diskid + "-l.dat")
+    self.identity = self.cat(self.diskid + "-i.dat").splitlines()
+    retm = retd = rets = ""
+    for line in self.identity:
+      if DEBUG:
+        print(line)
+      if (line != ''):
+        ls = line.split()
+        if (ls[0] == "Model"):
+          retm = line.split(': ')[1].strip()
+        if (ls[0] == "Device") and (ls[1] == "Model:"):
+          retd = line.split(': ')[1].strip()
+        if (ls[0] == "Serial"):
+          rets = line.split(': ')[1].strip()
+    self.identity = retm + " || " + retd + " (" + rets + ")"
+
+    if DEBUG:
+      print(self.identity)
+    return 0
+
+  def getdata(self, id):
+    ret = ""
+    for line in self.vars:
+      if (line != ''):
+        ls = line.split()
+        if (ls[0] == id):
+          if DEBUG:
+            print(line.split())
+          ret = ls[9]
+    return ret
+
+  def gethealth(self):
+    return self.health
+
+  def getlasttest(self):
+    return self.selftest
+
+  def getinfo(self):
+    return self.identity
+
+  def cat(self, filename):
+    ret = ""
+    if os.path.isfile(filename):
+      with open(filename, 'r') as f:
+        ret = f.read().strip('\n')
+    return ret
+
+if __name__ == '__main__':
+
+  DEBUG = True
+
+  print("**** Initialisation ****")
+  sda = SmartDisk("wwn-0x50026b723c0d6dd5")
+  print(" ")
+  print("**** smart() ****")
+  sda.smart()
+
+  print(" ")
+  print("**** getdata(194) ****")
+  print(sda.getdata('194'))
+
+  print(" ")
+  print("**** getlasttest() ****")
+  print(sda.getlasttest())
+
+  print(" ")
+  print("**** getinfo() ****")
+  print(sda.getinfo())
+
+  print(" ")
+  print("**** gethealth() ****")
+  print(sda.gethealth())
+
+  print(" ")
+  print("**** getdata(9) ****")
+  print(sda.getdata('9'))
