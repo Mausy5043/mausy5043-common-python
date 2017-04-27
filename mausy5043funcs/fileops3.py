945017d3fd1d8d832db4f8b5e0c95daa909147e0
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-04-27 13:00:42 +0200

    0.4.1: add logging function

--- mausy5043funcs/fileops3.py
+++ mausy5043funcs/fileops3.py
@@ -5,6 +5,7 @@ Provides file operation functions.
 """
 
 import os
+import syslog
 
 def lock(fname):
   """
@@ -18,3 +19,14 @@ def unlock(fname):
   """
   if os.path.isfile(fname):
     os.remove(fname)
+
+def syslog_trace(trace, logerr, out2console):
+  """
+  Log a (multi-line) message to syslog
+  """
+  log_lines = trace.split('\n')
+  for line in log_lines:
+    if line and logerr:
+      syslog.syslog(logerr, line)
+    if line and out2console:
+      print(line)
